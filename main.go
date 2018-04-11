a0c880469525140ea076a313cac3509eb5fac1bf
HelgyIngvarsson <olehgavrilin@gmail.com>
2018-04-11 17:54:41 +0300

    change port

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./assets/html/index.html")
 	})
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./assets/js"))))
-	http.ListenAndServe(":8000", nil)
+
+	port, _ := determineListenAddress()
+
+	http.ListenAndServe(port, nil)
+}
+
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
 }
